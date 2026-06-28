1db86f6a3579da4ec072908c8479367a9505c65f
Nephi Wright <nephiw@gmail.com>
2026-06-28 16:45:59 -0400

    Add whitenoise to allow static files to render

--- family_photos/settings.py
+++ family_photos/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     
     # Third party apps
     'django_htmx',
+    'whitenoise.runserver_nostatic',
     
     # Local apps
     'gallery.apps.GalleryConfig',
@@ -51,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -169,7 +171,7 @@ else:
             "BACKEND": "django.core.files.storage.FileSystemStorage",
         },
         "staticfiles": {
-            "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+            "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
         },
     }
 
