f7673f5abe7ff7264ae8da00cc3d2b0723a8eab5
Nephi Wright <nephiw@gmail.com>
2026-06-28 16:54:11 -0400

    Create an Admin account from env if there.

--- gallery/apps.py
+++ gallery/apps.py
@@ -1,6 +1,17 @@
+import os
+
 from django.apps import AppConfig
 
 
 class GalleryConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'gallery'
+
+    def ready(self):
+        username = os.getenv('ADMIN_USERNAME')
+        password = os.getenv('ADMIN_PASSWORD')
+        if username and password:
+            from django.contrib.auth.models import User
+            if not User.objects.filter(username=username).exists():
+                User.objects.create_superuser(username=username, password=password)
+                print(f"Admin user '{username}' created")
